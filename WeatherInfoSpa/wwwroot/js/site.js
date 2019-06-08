17607d23c525070b6e4e7b52196f7b3a08bbd878
Andrew Newcomb <anewcomb@capsahealthcare.com>
2019-06-08 14:58:53 -0700

    Started creating new website project (razor pages) that will hit the API endpoint.  Also, have successfully published API to azure.

---
+++ WeatherInfoSpa/wwwroot/js/site.js
@@ -0,0 +1,26 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+
+
+$('#zipcode').blur(function() {
+    var zipcode = $('#zipcode').val();
+    httpGet()
+});
+
+function httpGet(url, callback) {
+    var request = new XMLHttpRequest();
+
+    request.open('get', url, true);
+    request.onload = function() {
+        callback(request);
+    };
+
+    request.send();
+}
+
+httGet('details.txt',
+    function(request) {
+        console.log(request.responseText);
+    });
